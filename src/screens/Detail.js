172e575613cd668e918f6b5950ebdcd8eccad27c
amahh014 <nizammamedovv@gmail.com>
2020-03-27 15:42:32 +0100

    some changes
    some changes

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -2,52 +2,36 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 import { useQuery } from '@apollo/react-hooks'
-import {gql} from 'apollo-boost';
-
-const FETCH_POST_QUERY = gql`
-  query($postId: ID!) {
-    getPost(postId: $postId) {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
+import { FETCH_POST_QUERY } from './queries';
 
 const Detail = ({route}) => {
     const {postId} = route.params;
 
     const {
-        data
+        data,
+        loading,
+        error
       } = useQuery(FETCH_POST_QUERY, {
         variables: {
           postId
         }
       });
 
+      if (loading){
+        return <Text>Loading...</Text>
+      }
+
+      if (error) {
+        return <Text>Error occured</Text>
+      }
+
       const {
-        body,
-        username,
-        likeCount, 
-        unlikeCount, 
-        commentCount, 
+        body
       } = data.getPost;
 
         return (
             <View>
-                <Text>Hello</Text>
+                <Text>{body}</Text>
             </View>
         )
 }
